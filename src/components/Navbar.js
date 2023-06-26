47a6eedde84fb95f1167e2492958ca0ff31db609
Sushilendra G P <77796514+sushil540@users.noreply.github.com>
2023-06-27 01:03:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,7 +50,6 @@ const Navbar = (props) =>{
                          handleIsLoggedIn={handleIsLoggedIn}
                          exact/>}
                 } exact/>
-            <Route path="*" component={Dashboard}/>
             <PrivateRoute path="/dashboard" component={Dashboard} exact/>
             <ProtectedRoute path="/product" permitted={role} component={AddProduct} exact/> 
             <ProtectedRoute path="/setting" permitted={role} component={Settings} exact/>
